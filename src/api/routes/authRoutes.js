19a3699afba4ae8f1fba56f06008718b37b592e7
matteustirado <matteustirado@gmail.com>
2025-09-29 13:09:36 -0300

    fix: ajuste na pagina musics que nao estava fuincionando corretamente

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -6,11 +6,4 @@ const router = express.Router();
 
 router.post('/login', AuthController.login);
 
-router.get('/profile', authMiddleware, (request, response) => {
-    response.status(200).json({
-        message: 'Você está vendo uma rota protegida!',
-        user: request.user
-    });
-});
-
 module.exports = router;
